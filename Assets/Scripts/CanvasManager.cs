b1af125d5db4cae91e6d7ecb53b7cf4a882975b9
Mithat <mithatsirmali@gmail.com>
2025-09-19 19:31:55 +0300

    Main Menu Adjusting

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void ExitGame() =>
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+}
