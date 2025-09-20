4f6f0b0d8b6a4f612544c00ef6ecc1058056d7e7
Mithat <mithatsirmali@gmail.com>
2025-09-20 19:35:02 +0300

    Options scene has prepared

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public AudioSource musicSource;
+    public AudioMixer mixer;
     public Slider musicSlider;
 
     void Start()
     {
-        musicSlider.value = musicSource.volume;
+        musicSlider.minValue = 0.0001f;
+        musicSlider.maxValue = 1f;
+        musicSlider.value = 1f;
+
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
     }
 
-    public void SetMusicVolume(float volume)
+    public void SetMusicVolume(float value)
     {
-        musicSource.volume = volume;
+        float dB = Mathf.Log10(value) * 20f;
+        mixer.SetFloat("MusicVolume", dB);
     }
 }
